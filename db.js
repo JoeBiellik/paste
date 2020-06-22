ed142282e5acead92d58c6d1c6e780a62470d476
Joe Biellik <joebiellik@gmail.com>
2020-06-22 17:08:01 +0100

    Simplify DB configuration

--- db.js
+++ db.js
@@ -1,18 +1,12 @@
-const config = require('config');
-const util = require('util');
 const mongoose = require('mongoose');
+const config = require('config');
 
-module.exports = () => {
-	mongoose.Promise = global.Promise;
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-	mongoose.connection.once('open', util.log.bind(util, 'MongoDB connection open'));
-	mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connect(config.db, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
-	mongoose.set('useCreateIndex', true);
-
-	mongoose.connect(config.db, {
-		useNewUrlParser: true
-	});
-
-	return mongoose.connection;
-};
+module.exports = mongoose.connection;
