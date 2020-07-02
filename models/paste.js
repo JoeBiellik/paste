b4ab5d7ff3556f9a40c425afaa4dab5601b37027
Joe Biellik <joebiellik@gmail.com>
2020-07-02 20:11:08 +0100

    Remove default expiry on model

--- models/paste.js
+++ models/paste.js
@@ -4,7 +4,7 @@ const shortid = require('shortid');
 const paste = new mongoose.Schema({
 	_id: { type: String, default: shortid.generate },
 	paste: { type: String },
-	expiresAt: { type: Date, expires: 0, default: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) }
+	expiresAt: { type: Date, expires: 0 }
 }, {
 	timestamps: true
 });
