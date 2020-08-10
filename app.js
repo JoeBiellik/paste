0152b4f460c56275434a2f9afa9509892e5cd176
Joe Biellik <joebiellik@gmail.com>
2020-08-10 20:54:03 +0100

    Add security headers

--- app.js
+++ app.js
@@ -14,6 +14,30 @@ if (process.env.NODE_ENV == 'production') {
 	app.use(require('koa-logger')());
 }
 
+app.use(require('koa-helmet')({
+	hsts: false,
+	frameguard: {
+		action: 'deny'
+	},
+	referrerPolicy: {
+		policy: 'strict-origin'
+	},
+	contentSecurityPolicy: {
+		directives: {
+			'default-src': ["'none'"],
+			'base-uri': ["'none'"],
+			'connect-src': ["'self'"],
+			'font-src': ["'self'", 'https://fonts.gstatic.com'],
+			'form-action': ["'self'"],
+			'frame-ancestors': ["'none'"],
+			'img-src': ["'self'", 'https:', 'data:'],
+			'object-src': ["'none'"],
+			'script-src': ["'self'", 'https://cdnjs.cloudflare.com', 'https://code.jquery.com'],
+			'style-src': ["'self'", 'https://fonts.googleapis.com', 'https://cdnjs.cloudflare.com'],
+			'block-all-mixed-content': true
+		}
+	}
+}));
 app.use(require('koa-compress')());
 app.use(require('koa-static-cache')(path.join(__dirname, 'public'), {
 	maxAge: config.cacheAge
